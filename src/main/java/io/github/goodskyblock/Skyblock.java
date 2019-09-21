e0f703c93faa5c9fd972b3843f345ffd4d943ed7
alexanderwith <hyfe@yahoo.com>
2019-09-21 08:54:51 +0200

    Initial commit

---
+++ src/main/java/io/github/goodskyblock/Skyblock.java
@@ -0,0 +1,16 @@
+package io.github.goodskyblock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Skyblock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
